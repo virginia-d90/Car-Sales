c929c47bf68f459c39f7135d9e80a8adaae2ec7f
Virginia Davenport <virginialdport@gmail.com>
2020-06-09 15:45:54 -0600

    setup empty reducer and initialState. set up store and Provider.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
-import Total from './components/Total';
+import Total from './components/Total'; 
 
 const App = () => {
   const state = {
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import {combineReducers} from 'redux'
+import {allPowerfulReducer} from './allPowerfulReducer'
+
+export default combineReducers({
+    allPowerfulReducer,
+})
